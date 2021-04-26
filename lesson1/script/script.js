ee4f69556432d1de1916f5a75138b2e942ac6a83
oleksandr-hromovyi <alexander.gromovoj@gmail.com>
2021-04-26 17:22:14 +0300

    lesson1

---
+++ lesson1/script/script.js
@@ -0,0 +1,6 @@
+	 	document.write("<p>**************</p>");
+	 	document.write("<p>Обьем цилиндра с площадью основы S = ",10, " радиусом R = ",20,  " и высотой H = ",30, " равен:</p>");	
+	 	document.write("<p>--------------------</p>");
+	 	document.write("<p> V = ", Math.PI*(20**2)*30, "</p>");
+	 	document.write("<p>--------------------</p>");
+	 	document.write("<p>end.</p>");
