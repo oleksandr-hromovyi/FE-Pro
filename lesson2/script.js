cdf5229f54c1e5c7c1a2b9e4c15eab201ff0b5a9
oleksandr-hromovyi <alexander.gromovoj@gmail.com>
2021-05-04 12:56:31 +0300

    lesson 2

---
+++ lesson2/script.js
@@ -0,0 +1,34 @@
+sum = 100;
+
+a = +prompt('Enter A'); 
+if(isNaN(a) || a<=0){
+	alert(`A = ${a} is not valid number.`);
+	alert (`Sum = ${sum}`);
+} else{
+	alert(`A = ${a} is valid number.`);
+	sum += a;
+	alert (`Sum = ${sum}`);
+}
+
+b = +prompt('Enter B');
+if(isNaN(b) || b<=0){
+		alert(`B = ${b} is not valid number.`);
+	    alert (`Sum = ${sum}`);
+} else{
+	alert(`B = ${b} is valid number.`);
+	sum += b;
+	alert (`Sum = ${sum}`);
+}
+
+c = +prompt('Enter C');
+if(isNaN(c) || c<=0){
+		alert(`C = ${c} is not valid number.`);
+	    alert (`Sum = ${sum}`);
+} else{
+	alert(`C = ${c} is valid number.`);
+	sum += c;
+	alert (`Sum = ${sum}`);
+}
+
+
+
