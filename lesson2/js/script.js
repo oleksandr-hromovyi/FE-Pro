774ee8e696bac4555f2787f3086405e8a69ce336
oleksandr-hromovyi <alexander.gromovoj@gmail.com>
2021-05-07 11:20:16 +0300

    lesson 2 (upd)

--- lesson2/js/script.js
+++ lesson2/js/script.js
@@ -1,3 +1,4 @@
+/* first version
 sum = 100;
 
 a = +prompt('Enter A'); 
@@ -29,6 +30,35 @@ if(isNaN(c) || c<=0){
 	sum += c;
 	alert (`Sum = ${sum}`);
 }
+*/
 
+//upd
 
+sum = 100;
 
+a = prompt('Enter A'); 
+if(isNaN(a) || a<=0){
+	alert(`A = ${a} is not valid number.`);
+} else{
+	alert(`A = ${a} is valid number.`);
+	sum += +a;
+}
+alert (`Sum = ${sum}`);
+
+b = prompt('Enter B');
+if(isNaN(b) || b<=0){
+	alert(`B = ${b} is not valid number.`);
+} else{
+	alert(`B = ${b} is valid number.`);
+	sum += +b;
+}
+alert (`Sum = ${sum}`);
+
+c = prompt('Enter C');
+if(isNaN(c) || c<=0){
+	alert(`C = ${c} is not valid number.`);
+} else{
+	alert(`C = ${c} is valid number.`);
+	sum += +c;
+}
+alert (`Sum = ${sum}`);
