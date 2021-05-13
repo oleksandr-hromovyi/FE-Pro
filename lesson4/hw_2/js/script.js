bf21ca43aa048a776885ac1e4467915d56428f0e
oleksandr-hromovyi <alexander.gromovoj@gmail.com>
2021-05-14 00:00:54 +0300

    lesson4

--- lesson4/hw_2/js/script.js
+++ lesson4/hw_2/js/script.js
@@ -1,18 +1,30 @@
 //v.1
-/*
-sum = 0;
+/*sum = 0;
 for (i = 2; i <= 250; i++) {
 primeNumber = i;
-    for (j = 2; j<i ; j++)
-        {          
-             if (i%j===0) {
-               primeNumber = false;
-             }
-        }
-        if(primeNumber){
-               console.log(i);
-               sum = sum + i;
-             }
+  for (j = 2; j<i ; j++){          
+    if (i%j===0) {
+      primeNumber = false;
+    }
+    }
+  if(primeNumber){
+    console.log(i);
+    sum = sum + i;
+    }
 } 
 console.log(`Sum = ${sum}`);
 */
+
+
+//v.1.1
+sum = 0;
+nextCycle: for (i = 2; i <= 250; i++) {
+    for (j = 2; j < i; j++) {
+        if (i % j == 0) {
+          continue nextCycle;
+        } 
+    }
+    console.log(i);
+    sum = sum + i;
+}
+console.log(`Sum = ${sum}`);
