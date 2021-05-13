d93d1dd35621ece58786977d7b52d02fd8f75d10
oleksandr-hromovyi <alexander.gromovoj@gmail.com>
2021-05-13 23:15:56 +0300

    lesson4

---
+++ lesson4/hw_1/js/script.js
@@ -0,0 +1,44 @@
+//v1.0
+/*do {
+a = +prompt('Input a:');
+} while (isNaN(a));
+do {
+	b = +prompt('Input b, bigger than a:');
+} while (a >= b || isNaN(b)) ;
+do {
+h = +prompt('Input h:');
+} while (isNaN(h));
+
+function factorial(n) {
+  return (n != 1) ? n * factorial(n - 1) : 1;
+}
+
+sum = 0;
+
+while (a <= b) {
+	sum = sum + factorial(a);
+    a = a + h;
+}
+ alert(`Sum of factorials equal ${sum}`);
+ */
+
+ //v1.1
+do {
+a = +prompt('Input a:');
+} while (isNaN(a) || a<0);
+do {
+	b = +prompt('Input b, bigger than a:');
+} while (a >= b || isNaN(b) || b<0) ;
+do {
+h = +prompt('Input h:');
+} while (isNaN(h) || h<0 || h>b);
+sum = 0;
+for (; a<=b; a+=h) {
+factorial = 1;
+	for (n=1; n<=a; n++) {
+	factorial*=n;
+}
+	console.log(factorial);
+	sum+=factorial;
+}
+alert(`Sum of factorials equal ${sum}`);
---
+++ lesson4/hw_2/js/script.js
@@ -0,0 +1,18 @@
+//v.1
+/*
+sum = 0;
+for (i = 2; i <= 250; i++) {
+primeNumber = i;
+    for (j = 2; j<i ; j++)
+        {          
+             if (i%j===0) {
+               primeNumber = false;
+             }
+        }
+        if(primeNumber){
+               console.log(i);
+               sum = sum + i;
+             }
+} 
+console.log(`Sum = ${sum}`);
+*/
