33a1bcdc552567f2b887d27cff114a263b09bf39
oleksandr-hromovyi <alexander.gromovoj@gmail.com>
2021-06-15 22:48:32 +0300

    lesson14

--- lesson14/hw_3/js/script.js
+++ lesson14/hw_3/js/script.js
@@ -1,24 +1,24 @@
-/*Написать функцию assignObjects(obj1, obj2), которая принимает аргументами 2 объекта и возвращает новый, который состоит из свойств обоих объектов (склеить). 
-Если свойство повторяется, то взять значение второго объекта.
+/*Адаптировать функцию assignObjects() - из 2го задания - под неопределенное количество объектов. assignObjects(obj1, obj2, ....., objn);*/
 
-assignObjects({ x: 10, y: 20 }, { z: 30 }) -> { x:10, y:20, z: 30 }
-assignObjects({ x: 10 }, { x: 20, y: 30 }) - > { x:20, y: 30 } */
+let Obj1 = { x: 10, y: 20 }
+let Obj2 = { z: 30 }
 
-let firstObj1 = { x: 10, y: 20 }
-let secondObj1 = { z: 30 }
+let Obj3 = { x: 10 }
+let Obj4 = { x: 20, y: 30 };
 
-let firstObj2 = { x: 10 }
-let secondObj2 = { x: 20, y: 30 };
+let Obj5 = { x: 100 }
+let Obj6 = { x: 200, y: 300 };
 
-function assignObjects(obj1, obj2) {
-    let newObj = {}
-    if (typeof obj1 === "object" && Array.isArray(obj1) === false && typeof obj2 === "object" &&  Array.isArray(obj2) === false )  {
-
-    Object.assign(newObj, obj1, obj2);
+function assignObjects() {
+    if (typeof arguments === "object" && Array.isArray(arguments) === false)  {
+let newObj = {}
+for (let i=0; i<arguments.length; i++) {
+		    Object.assign(newObj, arguments[i]);
+   }
 
     console.log(newObj); } else {
     	return console.log(`something wrong, check  your objects`)
     }
 }
-assignObjects(firstObj1, secondObj1);
-assignObjects(firstObj2, secondObj2);
+assignObjects(Obj1, Obj2, Obj3, Obj4, Obj5, Obj6);
+
